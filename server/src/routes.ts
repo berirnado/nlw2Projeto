a09a1d6e4be7398ce20e8ea758877dcc9cb260a0
Bernardo <be.zito@hotmail.com>
2020-08-05 15:56:10 -0300

    criaçao de aulas 3/4

---
+++ server/src/routes.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import db from './database/connection';
+
+const routes = express.Router();
+
+routes.post('/classes', async (req, res, next) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = req.body;
+
+    const insertedUsersIds = await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+    });
+
+    const user_id = insertedUsersIds[0]
+
+    await db('classes').insert({
+        subject,
+        cost,
+        user_id,
+    })
+
+
+    return res.send();
+})
+
+export default routes;
